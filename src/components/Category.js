ee74024b38d2c197edf139a76e424522e664c884
Tushar <tusharansal23@gmail.com>
2024-05-05 07:17:41 +0530

    Category (event) changes

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,7 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import '../css/style.css';
 import { useEffect } from 'react';
+import event from '../img/event.jpg';
 
 const Category = () => {
 
@@ -8,11 +9,13 @@ const Category = () => {
     return (
         <div className="container-xxl py-5">
             <div className="container">
-                <div className="text-center category-container mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" /*style="max-width: 600px;"*/>
-                    <h1 className="mb-3">Events</h1>
-                    <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
-                </div>
-                <div className="row g-4">
+                <div className="mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" 
+                /*style="max-width: 600px;"*/>
+                    <h1 className="mb-3 ruby-text">Events</h1>
+                    <p>Join us for exciting events throughout the year, where you can engage with industry leaders, 
+                        network with like-minded professionals, and stay up-to-date with the latest trends and innovations in your field. Whether it's workshops, seminars, conferences, or networking sessions, there's something for everyone. Don't miss out on these valuable opportunities to learn, grow, and connect!</p>
+                    </div>
+                {/* <div className="row g-4">
                     <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
                         <a className="cat-item d-block bg-light text-center rounded p-3" href="">
                             <div className="rounded p-4">
@@ -101,6 +104,9 @@ const Category = () => {
                             </div>
                         </a>
                     </div>
+                </div> */}
+                <div className="row g-4">
+                <img className="img-fluid" src={event} alt="Icon"/>
                 </div>
             </div>
         </div>
